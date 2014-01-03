717b499cc83f2bfb8cd72841b2068b7e052f8c96
ezet <lars.dahl@gmail.com>
2014-01-03 21:03:19 +0100

    Added Register and get/set handling.

--- Z80 Emulator/Cpu.cs
+++ Z80 Emulator/Cpu.cs
@@ -4,8 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Word = System.Byte;
+using DWord = System.UInt16;
+
+
 namespace eZet.Z80.Emulator {
     class Cpu {
+
+        private Register register;
+
+        private FlagRegister flag = new FlagRegister();
+
+
+
+
 
 
     }
---
+++ Z80 Emulator/Register.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Word = System.Byte;
+using DWord = System.UInt16;
+
+namespace eZet.Z80.Emulator {
+
+
+    class Register {
+        public enum WordRegisterName { A, B, C, D, E, H, L, I, R }
+
+        public enum DWordRegisterName { AF, BC, DE, HL, AF2, BC2, DE2, HL2, IX, IY, SP, PC }
+
+        public enum StatusFlag { S, Z, I, H, P, C }
+
+        private int[] wordRegister = new int[9];
+
+        private int[] dWordRegister = new int[12];
+
+        private bool[] statusRegister = new bool[6];
+
+        public Word this[WordRegisterName index] {
+            get {
+                return (Word)wordRegister[(int)index];
+            }
+            set {
+                wordRegister[(int)index] = value;
+            }
+        }
+
+        public DWord this[DWordRegisterName index] {
+            get {
+                return (DWord)dWordRegister[(int)index];
+            }
+            set {
+                dWordRegister[(int)index] = value;
+            }
+        }
+
+        public bool this[StatusFlag index] {
+            get {
+                return statusRegister[(int)index];
+            }
+            set {
+                statusRegister[(int)index] = value;
+            }
+        }
+    }
+
+
+
+
+
+    }
+}
