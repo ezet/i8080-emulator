e9b650793411d96d8473394510487635a4e954fe
ezet <lars.dahl@gmail.com>
2014-01-06 22:44:10 +0100

    Implemented unit test for Register.

---
+++ Unit Test/RegisterTester.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+using eZet.i8080.Emulator;
+
+namespace eZet.i8080.Test {
+    [TestClass]
+    public class RegisterTester {
+
+        private Register reg = new Register();
+        private ushort b = 0xabcd;
+
+
+        [TestMethod]
+        public void TestWordRegister() {
+        }
+
+        [TestMethod]
+        public void TestDWordRegister() {
+
+        }
+
+
+
+
+
+
+    }
+}
