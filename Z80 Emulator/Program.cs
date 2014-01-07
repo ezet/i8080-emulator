4e6bb6d4465370913b216ff40446dc70606870fb
ezet <lars.dahl@gmail.com>
2014-01-07 02:47:09 +0100

    Executes invaders ROM for testing.

--- Z80 Emulator/Program.cs
+++ Z80 Emulator/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,11 @@ namespace eZet.i8080.Emulator {
     class Program {
         static void Main(string[] args) {
             Cpu cpu = new Cpu();
+            using (var fs = File.OpenRead("../../../invaders rom/invaders.h")) {
+                cpu.loadProgram(fs);
+                cpu.execute();
+            }
+
         }
     }
 }
